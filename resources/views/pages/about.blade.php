0131a7f682696545ff17e720c7db1e450eb7cac1
mafei <mafei27@hotmail.com>
2020-11-09 23:51:16 +0800

    update

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,6 +1,11 @@
 <x-main-layout>
   <div class="container mx-auto my-8">
-    <h1>about</h1>
+    <div class="prose lg:prose-xl">
+      <h1>about 123</h1>
+      @include('markdown.about')
+      <x-nav-link page="foo" text="bar" />
+    </div>
+
   </div>
 
 </x-main-layout>
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,34 @@ module.exports = {
         sans: ['Noto Sans HK', 'Noto Sans SC', ...defaultTheme.fontFamily.sans],
       },
     },
+    typography: (theme) => ({
+      default: {
+        css: {
+          maxWidth: 'none',
+          strong: {
+            fontWeight: '700'
+          },
+          'ul > li::before': {
+            backgroundColor: theme('colors.gray.500')
+          },
+          h1: {
+            color: theme('colors.gray.700')
+          },
+          h2: {
+            color: theme('colors.gray.700')
+          },
+          h3: {
+            color: theme('colors.gray.700')
+          },
+          h4: {
+            color: theme('colors.gray.700')
+          }
+        }
+      }
+    })
   },
   variants: {},
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography')
+  ],
 }
